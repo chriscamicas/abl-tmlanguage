1a14733142011151c93f45fc63676743f0c40726
Florent Colinet <colinetflorent@gmail.com>
2017-07-31 09:19:28 +0200

    fixe scope for define stream

--- spec/define-stream/simple-single-line.spec.js
+++ spec/define-stream/simple-single-line.spec.js
@@ -8,7 +8,8 @@ describe('', () => {
         { "startIndex": 0, "endIndex": 6, "scopes": ["source.abl", "meta.define.abl", "keyword.other.abl"] },
         { "startIndex": 6, "endIndex": 7, "scopes": ["source.abl", "meta.define.abl"] },
         { "startIndex": 7, "endIndex": 13, "scopes": ["source.abl", "meta.define.abl", "meta.define.stream.abl", "keyword.other.abl"] },
-        { "startIndex": 13, "endIndex": 21, "scopes": ["source.abl", "meta.define.abl", "meta.define.stream.abl"] },
+        { "startIndex": 13, "endIndex": 14, "scopes": ["source.abl", "meta.define.abl", "meta.define.stream.abl"] },
+        { "startIndex": 14, "endIndex": 21, "scopes": ["source.abl", "meta.define.abl", "meta.define.stream.abl", "variable.stream.abl"] },
         { "startIndex": 21, "endIndex": 22, "scopes": ["source.abl", "meta.define.abl", "punctuation.terminator.abl"] }
     ];
     shared.itShouldMatchExpectedScopes(grammar, statement, expectedTokens);
