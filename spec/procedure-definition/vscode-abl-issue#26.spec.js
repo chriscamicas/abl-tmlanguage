c4ae25920757cba6900be42cbc751874681af93f
Christophe Camicas <c.camicas@deal.fr>
2018-11-14 17:02:12 +0100

    fix chriscamicas/vscode-abl#26

---
+++ spec/procedure-definition/vscode-abl-issue#26.spec.js
@@ -0,0 +1,25 @@
+const { assert, expect } = require('chai');
+const shared = require('../shared.js');
+let grammar = shared.grammar;
+
+describe('', () => {
+    let statement =
+`procedure 'TpFrame.TaskPanel.ItemClick':U:
+
+end procedure.`;
+
+    let expectedTokens = [
+        [
+            {"startIndex":0,"endIndex":9,"scopes":["source.abl","meta.procedure.abl","keyword.other.abl"]},
+            {"startIndex":9,"endIndex":10,"scopes":["source.abl","meta.procedure.abl"]},
+            {"startIndex":10,"endIndex":41,"scopes":["source.abl","meta.procedure.abl","entity.name.function.abl"]},
+            {"startIndex":41,"endIndex":42,"scopes":["source.abl","meta.procedure.abl","meta.procedure.body.abl"]}
+        ],[
+            {"startIndex":0,"endIndex":1,"scopes":["source.abl","meta.procedure.abl","meta.procedure.body.abl"]}
+        ],[
+            {"startIndex":0,"endIndex":13,"scopes":["source.abl","meta.procedure.abl","meta.procedure.body.abl","keyword.other.abl"]},
+            {"startIndex":13,"endIndex":14,"scopes":["source.abl","punctuation.terminator.abl"]}
+        ]
+    ];
+    shared.itShouldMatchExpectedScopes(grammar, statement, expectedTokens);
+})
