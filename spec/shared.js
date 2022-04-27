85f873bc56a13914b77b26b37f62bbb6662283bd
Gilles QUERRET <g.querret@riverside-software.fr>
2022-04-27 14:45:16 +0200

    Upgrade dependencies - Node 16 (#15)
    * Upgrade dependencies - Node 16
    
    * Node 16 in Azure pipeline

--- spec/shared.js
+++ spec/shared.js
@@ -1,47 +1,39 @@
-const vsctm = require('vscode-textmate');
-const {
-    assert
-} = require('chai');
 const fs = require('fs');
+const path = require('path');
+const vsctm = require('vscode-textmate');
+const oniguruma = require('vscode-oniguruma');
+const { assert } = require('chai');
 
-const grammarPaths = {
-    'source.abl': './abl.tmLanguage.json'
-};
-let grammar;
+function readFile(p) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(p, (error, data) => error ? reject(error) : resolve(data));
+    })
+}
 
-let registry = new vsctm.Registry({
-    loadGrammar: function (scopeName) {
-        var path = grammarPaths[scopeName];
-        if (path) {
-            return new Promise((c, e) => {
-                fs.readFile(path, (error, content) => {
-                    if (error) {
-                        e(error);
-                    } else {
-                        var rawGrammar = vsctm.parseRawGrammar(content.toString(), path);
-                        c(rawGrammar);
-                    }
-                });
-            });
+const wasmBin = fs.readFileSync(path.join(__dirname, '../node_modules/vscode-oniguruma/release/onig.wasm')).buffer;
+const vscodeOnigurumaLib = oniguruma.loadWASM(wasmBin).then(() => {
+    return {
+        createOnigScanner(patterns) { return new oniguruma.OnigScanner(patterns); },
+        createOnigString(s) { return new oniguruma.OnigString(s); }
+    };
+});
+// Create a registry that can create a grammar from a scope name.
+const registry = new vsctm.Registry({
+    onigLib: vscodeOnigurumaLib,
+    loadGrammar: (scopeName) => {
+        if (scopeName === 'source.abl') {
+            return readFile('./abl.tmLanguage.json').then(data => vsctm.parseRawGrammar(data.toString(), './abl.tmLanguage.json'))
         }
+        console.log(`Unknown scope name: ${scopeName}`);
         return null;
     }
 });
 
-async function loadGrammar() {
-    if (!grammar)
-        return registry.loadGrammar('source.abl').then(g => grammar = g);
-    return Promise.resolve(grammar);
-};
-
 module.exports = {
-    get grammar() {
-        return undefined;
-    },
     itShouldMatchExpectedScopes: function (statement, expectedTokens) {
         describe(`${statement}`, () => {
             it('should match expected scopes', () => {
-                return loadGrammar().then(grammar => {
+                return registry.loadGrammar('source.abl').then(grammar => {
                     let tokenResult;
 
                     let lines = statement.split(/\n/g);
@@ -61,7 +53,6 @@ module.exports = {
                             tokenResult.tokens.push(r.tokens);
                         });
                     }
-                    // assert.includeDeepMembers(tokenResult.tokens, expectedTokens);
                     assert.deepEqual(tokenResult.tokens, expectedTokens, JSON.stringify(tokenResult.tokens));
                 })
             })
