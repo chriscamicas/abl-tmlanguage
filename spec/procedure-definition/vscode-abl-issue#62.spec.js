ced948351b5685a1b0d843badb947dee31ee8608
Peter Judge <peter@judge.co.za>
2023-05-21 12:24:36 -0400

    CAST/TYPE-OF improvements

--- spec/procedure-definition/vscode-abl-issue#62.spec.js
+++ spec/procedure-definition/vscode-abl-issue#62.spec.js
@@ -11,7 +11,8 @@ end procedure.`;
     [
       { "startIndex": 0, "endIndex": 9, "scopes": ["source.abl", "meta.procedure.abl", "keyword.other.abl"] },  // 'PROCEDURE'
       { "startIndex": 9, "endIndex": 10, "scopes": ["source.abl", "meta.procedure.abl"] },  // ' '
-      { "startIndex": 10, "endIndex": 26, "scopes": ["source.abl", "meta.procedure.abl", "entity.name.function.abl"] },  // 'EnumPrinters{&A}'
+      { "startIndex": 10, "endIndex": 22, "scopes": ["source.abl", "meta.procedure.abl", "entity.name.function.abl"] },  // 'EnumPrinters'
+      { "startIndex": 22, "endIndex": 26, "scopes": ["source.abl", "meta.procedure.abl", "storage.other.abl"] },  // '{&A}'
       { "startIndex": 26, "endIndex": 27, "scopes": ["source.abl", "meta.procedure.abl"] },  // ' '
       { "startIndex": 27, "endIndex": 35, "scopes": ["source.abl", "meta.procedure.abl", "keyword.other.abl"] },  // 'EXTERNAL'
       { "startIndex": 35, "endIndex": 36, "scopes": ["source.abl", "meta.procedure.abl"] },  // ' '
@@ -19,16 +20,16 @@ end procedure.`;
       { "startIndex": 37, "endIndex": 49, "scopes": ["source.abl", "meta.procedure.abl", "string.double.complex.abl"] },  // 'winspool.drv'
       { "startIndex": 49, "endIndex": 50, "scopes": ["source.abl", "meta.procedure.abl", "string.double.complex.abl", "punctuation.definition.string.end.abl"] },  // '"'
       { "startIndex": 50, "endIndex": 52, "scopes": ["source.abl"] }  // ':'
-    ],
-    [
+      ],
+      [
       { "startIndex": 0, "endIndex": 1, "scopes": ["source.abl"] }  // ''
-    ],
-    [
+      ],
+      [
       { "startIndex": 0, "endIndex": 3, "scopes": ["source.abl", "keyword.other.abl"] },  // 'end'
       { "startIndex": 3, "endIndex": 4, "scopes": ["source.abl"] },  // ' '
       { "startIndex": 4, "endIndex": 13, "scopes": ["source.abl", "keyword.other.abl"] },  // 'procedure'
       { "startIndex": 13, "endIndex": 14, "scopes": ["source.abl", "punctuation.terminator.abl"] }  // '.'
-    ]
+      ]    
   ];
   shared.itShouldMatchExpectedScopes(statement, expectedTokens);
 })
