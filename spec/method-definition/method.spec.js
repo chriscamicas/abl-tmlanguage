9812753154089d0f7cdc35a3b30a8c813a3bc9db
Peter Judge <peter@judge.co.za>
2024-03-16 16:06:20 -0400

    Make sure longchar is not seen as "long"

--- spec/method-definition/method.spec.js
+++ spec/method-definition/method.spec.js
@@ -183,3 +183,63 @@ end method.`;
   ];
   shared.itShouldMatchExpectedScopes(statement, expectedTokens);
 })
+
+describe('', () => {
+  let statement =
+    `method static public longchar Encode(input pString as longchar,
+      input pEscapeSeq as character):
+  message "a method".
+end method.`;
+
+  let expectedTokens = [
+    [
+      { "startIndex": 0, "endIndex": 6, "scopes": ["source.abl", "meta.define.method.abl", "keyword.other.abl"] },  // 'method'
+      { "startIndex": 6, "endIndex": 7, "scopes": ["source.abl", "meta.define.method.abl"] },  // ' '
+      { "startIndex": 7, "endIndex": 13, "scopes": ["source.abl", "meta.define.method.abl", "keyword.other.abl"] },  // 'static'
+      { "startIndex": 13, "endIndex": 14, "scopes": ["source.abl", "meta.define.method.abl"] },  // ' '
+      { "startIndex": 14, "endIndex": 20, "scopes": ["source.abl", "meta.define.method.abl", "keyword.other.abl"] },  // 'public'
+      { "startIndex": 20, "endIndex": 21, "scopes": ["source.abl", "meta.define.method.abl"] },  // ' '
+      { "startIndex": 21, "endIndex": 29, "scopes": ["source.abl", "meta.define.method.abl", "storage.type.abl"] },  // 'longchar'
+      { "startIndex": 29, "endIndex": 30, "scopes": ["source.abl", "meta.define.method.abl"] },  // ' '
+      { "startIndex": 30, "endIndex": 36, "scopes": ["source.abl", "meta.define.method.abl", "entity.name.function.abl"] },  // 'Encode'
+      { "startIndex": 36, "endIndex": 37, "scopes": ["source.abl", "meta.define.method.abl", "meta.brace.round.js"] },  // '('
+      { "startIndex": 37, "endIndex": 42, "scopes": ["source.abl", "meta.define.method.abl", "keyword.other.abl"] },  // 'input'
+      { "startIndex": 42, "endIndex": 43, "scopes": ["source.abl", "meta.define.method.abl"] },  // ' '
+      { "startIndex": 43, "endIndex": 50, "scopes": ["source.abl", "meta.define.method.abl", "variable.parameter.abl"] },  // 'pString'
+      { "startIndex": 50, "endIndex": 51, "scopes": ["source.abl", "meta.define.method.abl"] },  // ' '
+      { "startIndex": 51, "endIndex": 53, "scopes": ["source.abl", "meta.define.method.abl", "keyword.other.abl"] },  // 'as'
+      { "startIndex": 53, "endIndex": 54, "scopes": ["source.abl", "meta.define.method.abl"] },  // ' '
+      { "startIndex": 54, "endIndex": 58, "scopes": ["source.abl", "meta.define.method.abl", "storage.type.abl"] },  // 'long'
+      { "startIndex": 58, "endIndex": 62, "scopes": ["source.abl", "meta.define.method.abl"] },  // 'char'
+      { "startIndex": 62, "endIndex": 63, "scopes": ["source.abl", "meta.define.method.abl", "punctuation.separator.comma.abl"] }  // ','
+    ],
+    [
+      { "startIndex": 0, "endIndex": 6, "scopes": ["source.abl", "meta.define.method.abl"] },  // '      '
+      { "startIndex": 6, "endIndex": 11, "scopes": ["source.abl", "meta.define.method.abl", "keyword.other.abl"] },  // 'input'
+      { "startIndex": 11, "endIndex": 12, "scopes": ["source.abl", "meta.define.method.abl"] },  // ' '
+      { "startIndex": 12, "endIndex": 22, "scopes": ["source.abl", "meta.define.method.abl", "variable.parameter.abl"] },  // 'pEscapeSeq'
+      { "startIndex": 22, "endIndex": 23, "scopes": ["source.abl", "meta.define.method.abl"] },  // ' '
+      { "startIndex": 23, "endIndex": 25, "scopes": ["source.abl", "meta.define.method.abl", "keyword.other.abl"] },  // 'as'
+      { "startIndex": 25, "endIndex": 26, "scopes": ["source.abl", "meta.define.method.abl"] },  // ' '
+      { "startIndex": 26, "endIndex": 35, "scopes": ["source.abl", "meta.define.method.abl", "storage.type.abl"] },  // 'character'
+      { "startIndex": 35, "endIndex": 36, "scopes": ["source.abl", "meta.define.method.abl", "meta.brace.round.js"] },  // ')'
+      { "startIndex": 36, "endIndex": 38, "scopes": ["source.abl"] }  // ':'
+    ],
+    [
+      { "startIndex": 0, "endIndex": 2, "scopes": ["source.abl"] },  // '  '
+      { "startIndex": 2, "endIndex": 9, "scopes": ["source.abl", "keyword.other.abl"] },  // 'message'
+      { "startIndex": 9, "endIndex": 10, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 10, "endIndex": 11, "scopes": ["source.abl", "string.double.complex.abl", "punctuation.definition.string.begin.abl"] },  // '"'
+      { "startIndex": 11, "endIndex": 19, "scopes": ["source.abl", "string.double.complex.abl"] },  // 'a method'
+      { "startIndex": 19, "endIndex": 20, "scopes": ["source.abl", "string.double.complex.abl", "punctuation.definition.string.end.abl"] },  // '"'
+      { "startIndex": 20, "endIndex": 21, "scopes": ["source.abl", "punctuation.terminator.abl"] }  // '.'
+    ],
+    [
+      { "startIndex": 0, "endIndex": 3, "scopes": ["source.abl", "keyword.other.abl"] },  // 'end'
+      { "startIndex": 3, "endIndex": 4, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 4, "endIndex": 10, "scopes": ["source.abl", "keyword.other.abl"] },  // 'method'
+      { "startIndex": 10, "endIndex": 11, "scopes": ["source.abl", "punctuation.terminator.abl"] }  // '.'
+    ]
+  ];
+  shared.itShouldMatchExpectedScopes(statement, expectedTokens);
+})
