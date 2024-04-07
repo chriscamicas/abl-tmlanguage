d62b1d83c63a0a7eb2cae11bbddaaaf75ae5608a
Peter Judge <peter@judge.co.za>
2024-04-07 19:55:28 -0400

    Revert "Allow preprocessors in more statements/places"
    This reverts commit d16cd1af7e726dcabc82c1350caa1893756ebee5.

--- spec/analyze-suspend-resume/analyze-suspend-resume.spec.js
+++ spec/analyze-suspend-resume/analyze-suspend-resume.spec.js
@@ -8,12 +8,10 @@ describe('', () => {
 
   let expectedTokens = [
     [
-      { "startIndex": 0, "endIndex": 17, "scopes": ["source.abl", "comment.preprocessor.analyze-suspend.abl"] },  // '&ANALYZE-SUSPEND '
-      { "startIndex": 17, "endIndex": 42, "scopes": ["source.abl", "comment.preprocessor.analyze-suspend.abl"] }  // '_VERSION-NUMBER AB_v10r12'
-    ],
-    [
-      { "startIndex": 0, "endIndex": 16, "scopes": ["source.abl", "comment.preprocessor.analyze-suspend.abl"] }  // '&ANALYZE-RESUME'
-    ]
-  ];
+      { "startIndex": 0, "endIndex": 17, "scopes": ["source.abl", "comment.preprocessor.analyze-suspend.abl"] },
+      { "startIndex": 17, "endIndex": 42, "scopes": ["source.abl", "comment.preprocessor.analyze-suspend.abl"] }
+    ], [
+      { "startIndex": 0, "endIndex": 16, "scopes": ["source.abl", "comment.preprocessor.analyze-suspend.abl"] }
+    ]];
   shared.itShouldMatchExpectedScopes(statement, expectedTokens);
 })
