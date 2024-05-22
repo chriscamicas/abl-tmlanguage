40a4e7810ba955631419c73a0b09404eb83e235a
Peter Judge <peter@judge.co.za>
2024-05-21 20:35:59 -0400

    Support comments in IF-THEN statements

--- spec/define-stream/simple-single-line.spec.js
+++ spec/define-stream/simple-single-line.spec.js
@@ -5,7 +5,7 @@ describe('', () => {
   let statement = `define stream fileLog.`;
   let expectedTokens = [
     { "startIndex": 0, "endIndex": 6, "scopes": ["source.abl", "meta.define.abl", "keyword.other.abl"] },  // 'define'
-    { "startIndex": 6, "endIndex": 7, "scopes": ["source.abl", "meta.define.abl"] },  // ' '
+    { "startIndex": 6, "endIndex": 7, "scopes": ["source.abl", "meta.define.abl", "meta.define.stream.abl"] },  // ' '
     { "startIndex": 7, "endIndex": 13, "scopes": ["source.abl", "meta.define.abl", "meta.define.stream.abl", "keyword.other.abl"] },  // 'stream'
     { "startIndex": 13, "endIndex": 14, "scopes": ["source.abl", "meta.define.abl", "meta.define.stream.abl"] },  // ' '
     { "startIndex": 14, "endIndex": 21, "scopes": ["source.abl", "meta.define.abl", "meta.define.stream.abl", "variable.other.abl"] },  // 'fileLog'
