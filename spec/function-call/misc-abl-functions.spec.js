a53e264aa68dc9253c766f6dee3c0e673ace0749
Peter Judge <peter@judge.co.za>
2024-11-13 23:00:06 -0500

    Allow output as a variable name (see webspeed)

---
+++ spec/function-call/misc-abl-functions.spec.js
@@ -0,0 +1,22 @@
+const { assert, expect } = require('chai');
+const shared = require('../shared.js');
+
+describe('', () => {
+  let statement = `if can-do("data",output-content-type) then.`;
+  let expectedTokens = [
+    { "startIndex": 0, "endIndex": 2, "scopes": ["source.abl", "keyword.other.abl"] },  // 'if'
+    { "startIndex": 2, "endIndex": 3, "scopes": ["source.abl"] },  // ' '
+    { "startIndex": 3, "endIndex": 9, "scopes": ["source.abl", "meta.function-call.abl", "support.function.abl"] },  // 'can-do'
+    { "startIndex": 9, "endIndex": 10, "scopes": ["source.abl", "meta.function-call.abl", "meta.function.arguments.abl", "meta.brace.round.js"] },  // '('
+    { "startIndex": 10, "endIndex": 11, "scopes": ["source.abl", "meta.function-call.abl", "meta.function.arguments.abl", "string.double.complex.abl", "punctuation.definition.string.begin.abl"] },  // '"'
+    { "startIndex": 11, "endIndex": 15, "scopes": ["source.abl", "meta.function-call.abl", "meta.function.arguments.abl", "string.double.complex.abl"] },  // 'data'
+    { "startIndex": 15, "endIndex": 16, "scopes": ["source.abl", "meta.function-call.abl", "meta.function.arguments.abl", "string.double.complex.abl", "punctuation.definition.string.end.abl"] },  // '"'
+    { "startIndex": 16, "endIndex": 17, "scopes": ["source.abl", "meta.function-call.abl", "meta.function.arguments.abl", "punctuation.separator.comma.abl"] },  // ','
+    { "startIndex": 17, "endIndex": 36, "scopes": ["source.abl", "meta.function-call.abl", "meta.function.arguments.abl", "variable.other.abl"] },  // 'output-content-type'
+    { "startIndex": 36, "endIndex": 37, "scopes": ["source.abl", "meta.function-call.abl", "meta.brace.round.js"] },  // ')'
+    { "startIndex": 37, "endIndex": 38, "scopes": ["source.abl"] },  // ' '
+    { "startIndex": 38, "endIndex": 42, "scopes": ["source.abl", "keyword.other.abl"] },  // 'then'
+    { "startIndex": 42, "endIndex": 43, "scopes": ["source.abl", "punctuation.terminator.abl"] }  // '.'
+  ];
+  shared.itShouldMatchExpectedScopes(statement, expectedTokens);
+})
