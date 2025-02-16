9ef1fe195e3c2673edfeba2b50dc4701e36650cf
Peter Judge <peter@judge.co.za>
2025-02-16 17:08:20 -0500

    Improve scoping for method names, language functions in IF statements, DO block modifiers

--- spec/method-attribute-property-call/method-call.spec.js
+++ spec/method-attribute-property-call/method-call.spec.js
@@ -173,3 +173,96 @@ end.`;
   ];
   shared.itShouldMatchExpectedScopes(statement, expectedTokens);
 })
+
+describe('', () => {
+  let statement = `if Progress.Lang.Class:GetClass (pcEntityName):IsA ( "Consultingwerk.OERA.BusinessTask":U) or
+(Progress.Lang.Class:GetClass (pcEntityName):IsA ( "Consutingwerk.OERA.IBusinessService":U) and
+Progress.Lang.Class:GetClass (pcEntityName) :IsA ( "Consultingwerk.SmartFramework.SmartBusinessEntity":U) eq false) then
+  poRestResourceService:RegisterBusinessTaskRestMethod (oMethod).`;
+  let expectedTokens = [
+    [
+      { "startIndex": 0, "endIndex": 2, "scopes": ["source.abl", "keyword.other.abl"] },  // 'if'
+      { "startIndex": 2, "endIndex": 3, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 3, "endIndex": 22, "scopes": ["source.abl", "entity.name.type.abl"] },  // 'Progress.Lang.Class'
+      { "startIndex": 22, "endIndex": 23, "scopes": ["source.abl", "punctuation.separator.colon.abl"] },  // ':'
+      { "startIndex": 23, "endIndex": 31, "scopes": ["source.abl", "entity.name.function.abl"] },  // 'GetClass'
+      { "startIndex": 31, "endIndex": 32, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 32, "endIndex": 33, "scopes": ["source.abl", "meta.brace.round.js"] },  // '('
+      { "startIndex": 33, "endIndex": 45, "scopes": ["source.abl", "variable.other.abl"] },  // 'pcEntityName'
+      { "startIndex": 45, "endIndex": 46, "scopes": ["source.abl", "meta.brace.round.js"] },  // ')'
+      { "startIndex": 46, "endIndex": 47, "scopes": ["source.abl", "punctuation.separator.colon.abl"] },  // ':'
+      { "startIndex": 47, "endIndex": 50, "scopes": ["source.abl", "entity.name.function.abl"] },  // 'IsA'
+      { "startIndex": 50, "endIndex": 51, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 51, "endIndex": 52, "scopes": ["source.abl", "meta.brace.round.js"] },  // '('
+      { "startIndex": 52, "endIndex": 53, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 53, "endIndex": 54, "scopes": ["source.abl", "string.double.complex.abl", "punctuation.definition.string.begin.abl"] },  // '"'
+      { "startIndex": 54, "endIndex": 86, "scopes": ["source.abl", "string.double.complex.abl"] },  // 'Consultingwerk.OERA.BusinessTask'
+      { "startIndex": 86, "endIndex": 87, "scopes": ["source.abl", "string.double.complex.abl", "punctuation.definition.string.end.abl"] },  // '"'
+      { "startIndex": 87, "endIndex": 89, "scopes": ["source.abl", "string.double.complex.abl", "support.other.abl"] },  // ':U'
+      { "startIndex": 89, "endIndex": 90, "scopes": ["source.abl", "meta.brace.round.js"] },  // ')'
+      { "startIndex": 90, "endIndex": 91, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 91, "endIndex": 93, "scopes": ["source.abl", "keyword.other.abl"] }  // 'or'
+    ],
+    [
+      { "startIndex": 0, "endIndex": 1, "scopes": ["source.abl", "meta.brace.round.js"] },  // '('
+      { "startIndex": 1, "endIndex": 20, "scopes": ["source.abl", "entity.name.type.abl"] },  // 'Progress.Lang.Class'
+      { "startIndex": 20, "endIndex": 21, "scopes": ["source.abl", "punctuation.separator.colon.abl"] },  // ':'
+      { "startIndex": 21, "endIndex": 29, "scopes": ["source.abl", "entity.name.function.abl"] },  // 'GetClass'
+      { "startIndex": 29, "endIndex": 30, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 30, "endIndex": 31, "scopes": ["source.abl", "meta.brace.round.js"] },  // '('
+      { "startIndex": 31, "endIndex": 43, "scopes": ["source.abl", "variable.other.abl"] },  // 'pcEntityName'
+      { "startIndex": 43, "endIndex": 44, "scopes": ["source.abl", "meta.brace.round.js"] },  // ')'
+      { "startIndex": 44, "endIndex": 45, "scopes": ["source.abl", "punctuation.separator.colon.abl"] },  // ':'
+      { "startIndex": 45, "endIndex": 48, "scopes": ["source.abl", "entity.name.function.abl"] },  // 'IsA'
+      { "startIndex": 48, "endIndex": 49, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 49, "endIndex": 50, "scopes": ["source.abl", "meta.brace.round.js"] },  // '('
+      { "startIndex": 50, "endIndex": 51, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 51, "endIndex": 52, "scopes": ["source.abl", "string.double.complex.abl", "punctuation.definition.string.begin.abl"] },  // '"'
+      { "startIndex": 52, "endIndex": 87, "scopes": ["source.abl", "string.double.complex.abl"] },  // 'Consutingwerk.OERA.IBusinessService'
+      { "startIndex": 87, "endIndex": 88, "scopes": ["source.abl", "string.double.complex.abl", "punctuation.definition.string.end.abl"] },  // '"'
+      { "startIndex": 88, "endIndex": 90, "scopes": ["source.abl", "string.double.complex.abl", "support.other.abl"] },  // ':U'
+      { "startIndex": 90, "endIndex": 91, "scopes": ["source.abl", "meta.brace.round.js"] },  // ')'
+      { "startIndex": 91, "endIndex": 92, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 92, "endIndex": 95, "scopes": ["source.abl", "keyword.other.abl"] }  // 'and'
+    ],
+    [
+      { "startIndex": 0, "endIndex": 19, "scopes": ["source.abl", "entity.name.type.abl"] },  // 'Progress.Lang.Class'
+      { "startIndex": 19, "endIndex": 20, "scopes": ["source.abl", "punctuation.separator.colon.abl"] },  // ':'
+      { "startIndex": 20, "endIndex": 28, "scopes": ["source.abl", "entity.name.function.abl"] },  // 'GetClass'
+      { "startIndex": 28, "endIndex": 29, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 29, "endIndex": 30, "scopes": ["source.abl", "meta.brace.round.js"] },  // '('
+      { "startIndex": 30, "endIndex": 42, "scopes": ["source.abl", "variable.other.abl"] },  // 'pcEntityName'
+      { "startIndex": 42, "endIndex": 43, "scopes": ["source.abl", "meta.brace.round.js"] },  // ')'
+      { "startIndex": 43, "endIndex": 44, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 44, "endIndex": 45, "scopes": ["source.abl", "punctuation.separator.colon.abl"] },  // ':'
+      { "startIndex": 45, "endIndex": 48, "scopes": ["source.abl", "entity.name.function.abl"] },  // 'IsA'
+      { "startIndex": 48, "endIndex": 49, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 49, "endIndex": 50, "scopes": ["source.abl", "meta.brace.round.js"] },  // '('
+      { "startIndex": 50, "endIndex": 51, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 51, "endIndex": 52, "scopes": ["source.abl", "string.double.complex.abl", "punctuation.definition.string.begin.abl"] },  // '"'
+      { "startIndex": 52, "endIndex": 101, "scopes": ["source.abl", "string.double.complex.abl"] },  // 'Consultingwerk.SmartFramework.SmartBusinessEntity'
+      { "startIndex": 101, "endIndex": 102, "scopes": ["source.abl", "string.double.complex.abl", "punctuation.definition.string.end.abl"] },  // '"'
+      { "startIndex": 102, "endIndex": 104, "scopes": ["source.abl", "string.double.complex.abl", "support.other.abl"] },  // ':U'
+      { "startIndex": 104, "endIndex": 105, "scopes": ["source.abl", "meta.brace.round.js"] },  // ')'
+      { "startIndex": 105, "endIndex": 106, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 106, "endIndex": 108, "scopes": ["source.abl", "keyword.operator.source.abl"] },  // 'eq'
+      { "startIndex": 108, "endIndex": 109, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 109, "endIndex": 114, "scopes": ["source.abl", "constant.language.abl"] },  // 'false'
+      { "startIndex": 114, "endIndex": 115, "scopes": ["source.abl", "meta.brace.round.js"] },  // ')'
+      { "startIndex": 115, "endIndex": 116, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 116, "endIndex": 120, "scopes": ["source.abl", "keyword.other.abl"] }  // 'then'
+    ],
+    [
+      { "startIndex": 0, "endIndex": 2, "scopes": ["source.abl"] },  // '  '
+      { "startIndex": 2, "endIndex": 23, "scopes": ["source.abl", "variable.other.abl"] },  // 'poRestResourceService'
+      { "startIndex": 23, "endIndex": 24, "scopes": ["source.abl", "punctuation.separator.colon.abl"] },  // ':'
+      { "startIndex": 24, "endIndex": 54, "scopes": ["source.abl", "entity.name.function.abl"] },  // 'RegisterBusinessTaskRestMethod'
+      { "startIndex": 54, "endIndex": 55, "scopes": ["source.abl"] },  // ' '
+      { "startIndex": 55, "endIndex": 56, "scopes": ["source.abl", "meta.function.arguments.abl", "meta.brace.round.js"] },  // '('
+      { "startIndex": 56, "endIndex": 63, "scopes": ["source.abl", "meta.function.arguments.abl", "variable.other.abl"] },  // 'oMethod'
+      { "startIndex": 63, "endIndex": 64, "scopes": ["source.abl", "meta.brace.round.js"] },  // ')'
+      { "startIndex": 64, "endIndex": 65, "scopes": ["source.abl", "punctuation.terminator.abl"] }  // '.'
+    ]
+  ];
+  shared.itShouldMatchExpectedScopes(statement, expectedTokens);
+})
